3fd2a614c009802483924e88e7ad3e42bbafe511
sshhaahh <ameenfalak786@gmail.com>
2024-11-05 16:02:21 +0530

    Ready to test

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const winCon = [
     [0,3,6],
     [1,4,7],
     [0,4,8],
-    [2,5,7]    
+    [2,5,8]    
 ];
 
 
@@ -23,6 +23,14 @@ function initGame(){
     gameGrid = ["","","","","","","","",""];
     btn.classList.remove("active");
     gameInfo.innerHTML = `Current Player - ${currentPlayer}`;
+    boxes.forEach((box,index) => {
+        box.innerHTML = "";
+        boxes[index].style.pointerEvents="all";
+        boxes[index].style.cursor = 'pointer';
+
+        boxes[index].classList.remove("win");
+        
+    })
     
     
 }
@@ -40,6 +48,47 @@ function swapTurn(){
 }
 
 function checkGameover(){
+    let answer = "";
+    winCon.forEach(position=>{
+        if(
+            (gameGrid[position[0]]!==""||gameGrid[position[1]]!==""||gameGrid[position[2]]!=="") &&
+            (gameGrid[position[0]]===gameGrid[position[1]])&&(gameGrid[position[1]]===gameGrid[position[2]])
+        ){
+            if(gameGrid[position[0]]==="X"){
+                answer="X";
+            }else{
+                answer="0";
+            }
+            boxes.forEach(box=>{
+                box.style.pointerEvents="none";
+            });
+
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+            
+        }
+
+       
+
+        if(answer!==""){
+            gameInfo.innerText=`Winner is - ${answer}`;
+            btn.classList.add("active");            
+            return;
+        }
+
+        let fillCount = 0;
+        gameGrid.forEach(box=>{
+            if(box!==""){
+                fillCount++;
+            }
+        });
+        if(fillCount===9){
+            gameInfo.innerText = "Game Tied!";
+            btn.classList.add("active");
+
+        }
+    });
    
 }
 
